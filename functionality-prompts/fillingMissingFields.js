2f1d2e6fcd7f527e773ca4b8095128deee622499
Rajibul Islam <rajibulski110@gmail.com>
2024-12-20 03:59:22 +0530

    prompt optimization

--- functionality-prompts/fillingMissingFields.js
+++ functionality-prompts/fillingMissingFields.js
@@ -52,7 +52,13 @@ Here, the note is omitted as it's not relevant.
 - If you provided the second sentence which is simpler and fill the beginner and upper beginner and middle intermidiate needs, make the third sentence longer, not poetic, but how I described about making it complex but not complicated, and practical and not poetic, and how there should be a lot of reading comprehension value, that would regardless the level defined, be useful for upper-beginners and intermediates.
 - Grammar type cards can have 10 example fields, I want most of them to be beginner-friendly stretching to intermediate and upper-intermediate. Don't make these long, little big mid sized is good, but don't treat these as the vocab deck ex3s that are long.
 - You must stop and think whether a the 'note' should be important for the instence. And then provide it if it is important.
-- While in instruction I gave 3 example possibility as max, big amounts like 10 example field request can occur and you need to perfectly fill all 10. Make sure all the sentences are not too similar to each other, for reading comprehension, you can have little similar as that can rather help than all random. The examples can ascend in length depending on the number of sentences, but too long would be bad, mid long is good, avoiding one liner short all over is what I want you to avoid.`;
+- While in instruction I gave 3 example possibility as max, big amounts like 10 example field request can occur and you need to perfectly fill all 10. Make sure all the sentences are not too similar to each other, for reading comprehension, you can have little similar as that can rather help than all random. The examples can ascend in length depending on the number of sentences, but too long would be bad, mid long is good, avoiding one liner short all over is what I want you to avoid.
+
+The JSON should correctly escape when it needs to have the double quotes (") usage inside some values, by that I mean inside a value, the necessary quote wrapper for keys and values are always there, I'm talking about inside any value's double quotes if you happen to generate some fields like that.
+
+Make sure we don't run into this error when parsing: Unexpected token ']\`', "\`\`\`json
+{
+"... is not valid JSON`;
 
 export const fixNotesAndExplanationFieldsInJapanese = `Turn the 'explanation' and 'note' fields in English for my Japanese learning Anki deck, I will provide with the card details, you have to provide the 'note' and 'explanation' fields in english as a JSON object with required keys and string values.
 
@@ -65,4 +71,10 @@ The JSON Object should be like:
 If there's no notes or explanation fields or the value is "", don't add that into the JSON object. That can mean, the object can have only one key value pair possibility, if both of them aren't there in the prompt, return to me an empty object {}. If the 'note' and 'explanation' is already in English, don't add them in the your JSON object, which if both fields are in English, should cause you to provide an empty object.
 
 Similarly, I can give you vocabulary card which doesn't have the 'explanation' field but has the 'note' field, please turn it into English. Same rules, if the note field is already in English or there's nothing in the note field, provide empty object {}.
+
+The JSON should correctly escape when it needs to have the double quotes (") usage inside some values, by that I mean inside a value, the necessary quote wrapper for keys and values are always there, I'm talking about inside any value's double quotes if you happen to generate some fields like that.
+
+Make sure we don't run into this error when parsing: Unexpected token ']\`', "\`\`\`json
+{
+"... is not valid JSON
 `;
